463b6e807612509e76d5b216f19261834c766ef7
alekslesik <alekslesik@gmail.com>
2022-05-17 22:17:53 +0300

    first

---
+++ services.go
@@ -0,0 +1,35 @@
+package service
+
+import "context"
+
+type (
+	Service interface {
+		// Init tries to perform the initial initialization of the service, the logic of the function must make sure
+		// that all created connections to remote services are in working order and are pinging. Otherwise, the
+		// application will need additional error handling.
+		Init(ctx context.Context) error
+		// Ping will be called by the service controller at regular intervals, it is important that a response with
+		// any error will be regarded as an unrecoverable state of the service and will lead to an emergency stop of
+		// the application. If the service is not critical for the application, like a memcached, then try to implement
+		// the logic of self-diagnosis and service recovery inside Ping, and return the nil as a response even if the
+		// recovery failed.
+		Ping(ctx context.Context) error
+		// Close will be executed when the service controller receives a stop command. Normally, this happens after the
+		// main thread of the application has already finished. That is, no more requests from the outside are expected.
+		Close() error
+	}
+
+	ServiceKeeper struct {
+		Services []Service
+		state int32 //for control executing stages
+	}
+)
+
+func (s *ServiceKeeper) initAllServices(ctx context.Context) error {
+	for i := range s.Services {
+		if err := s.Services[i].Init(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
